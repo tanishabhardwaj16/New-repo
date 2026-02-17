98372dfe726d70166323939b5ffbf27b3a3e8750
Tanisha Bhardwaj <tanisha@Tanishas-MacBook-Air-2.local>
2026-02-17 18:53:49 +0530

    hello

---
+++ gitNotes.js
@@ -0,0 +1,18 @@
+// Git -> Version control that allows you to track changes in your code and collaborate with others.
+
+// GitHub -> A web-based platform that hosts Git repositories and provides additional features for collaboration and project management.
+
+// Git Commands:
+
+// git init -> Initializes a new Git repository in the current directory.
+//create a folder --> mkdir newFolder
+//create a file --> touch newFile.txt
+// .git --> hidden folder that contains all the information about the Git repository, including commit history, branches, and configuration.
+
+// git add -> Stages changes to be committed. You can specify individual files or use '.' to stage all changes.
+// git commit -m "commit message" -> take snapshot of your project and save the history of changes. The -m flag allows you to provide a commit message describing the changes made in that commit.
+// git status -> Shows the current status of the repository, including staged and unstaged changes.
+// git add. --> add all files and folder to staging area
+//Untracked files -> Files that are not being tracked by Git. They have not been added to the staging area or committed yet.
+//staged files -> Files that have been added to the staging area and are ready to be committed. They have been marked for inclusion in the next commit.
+//files to be committed -> Files that have been staged and are ready to be included in the next commit. These files will be part of the commit history once the commit is made.
